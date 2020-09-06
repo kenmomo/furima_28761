0a6057cb0fec9941763ea39b5f7de1a1d6eecc06
kenmomo <momoken0209@gmail.com>
2020-09-06 21:11:10 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_furigana, :first_name_furigana, :email, :encrypted_password, :birthday ])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
