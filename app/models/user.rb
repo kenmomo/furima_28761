de3064012faca2f268095ce199d97a3d44ee3972
kenmomo <momoken0209@gmail.com>
2020-09-07 17:00:02 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,19 @@ class User < ApplicationRecord
           validates :encrypted_password
           validates :birthday
           end
+
+          validates :email, uniqueness: true
+
+          VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/
+          validates :password,    length: { minimum: 6 },
+          format:{ with: VALID_PASSWORD_REGEX }
+
+          validates :last_name, format:{ with: /\A[ぁ-んァ-ン一-龥]/ }
+
+          validates :first_name, format:{ with: /\A[ぁ-んァ-ン一-龥]/ }
+
+          validates :last_name_furigana, format:{ with: /\A[ァ-ヶー－]+\z/ }
+
+          validates :first_name_furigana, format:{ with: /\A[ァ-ヶー－]+\z/ }
+
 end
