0150adfe21810fc049ddd0bc92d6efed326f8802
kenmomo <momoken0209@gmail.com>
2020-09-07 22:03:59 +0900

    テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"abe"}
+    email                 {Faker::Internet.free_email}
+    password              {"00000000"}
+    password_confirmation {password}
+    last_name             {"あべ"}
+    first_name            {"たろう"}
+    last_name_furigana    {"アベ"}
+    first_name_furigana   {"タロウ"}
+    birthday              {"1996-02-09"}
+    
+  end
+end
