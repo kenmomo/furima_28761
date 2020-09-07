17a831246706caaff7d1845bde82091336f04fce
kenmomo <momoken0209@gmail.com>
2020-09-07 11:52:11 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+          validates :nickname
+          validates :last_name
+          validates :first_name
+          validates :last_name_furigana
+          validates :first_name_furigana
+          validates :email
+          validates :encrypted_password
+          validates :birthday
+          end
 end
