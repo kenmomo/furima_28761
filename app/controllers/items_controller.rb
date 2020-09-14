10c14a53c6c20463e42b3c1e3259dfb9ae08b2d2
kenmomo <momoken0209@gmail.com>
2020-09-14 17:35:41 +0900

    商品削除機能実装直し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,8 +40,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if @item.destroy
     redirect_to root_path
+    else
+      render :show
+    end
+
   end
 
   private
